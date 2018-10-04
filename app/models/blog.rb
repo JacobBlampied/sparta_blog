855dbe386acd6aecf87abcbea9f2e53eded6abc3
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-10-04 07:55:54 +0100

    added validations to form

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,5 @@ has_many :comments
   validates :title, :presence => true
   validates :body, :presence => true
   validates :image, :presence => true
+  validates :title, length: { maximum: 20 }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
+
+  validates :body, length: { maximum: 100 }
+  validates :title, :presence => true
 end
